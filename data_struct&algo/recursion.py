39ae1d647361acb8e2c6bdb7866b57ba5d4db0fb
raj-hegde <rajeevhegde11@gmail.com>
2022-06-25 23:54:31 +0530

    missed print statement

--- data_struct&algo/recursion.py
+++ data_struct&algo/recursion.py
@@ -1,7 +1,7 @@
 def factorial(n):
     if n == 0:
         return 1
-    return n * factorial(n - 1)
+    else:
+        return n * factorial(n - 1)
 
-num = 3
-factorial(num)
+print(f"factorial is" , factorial(7))

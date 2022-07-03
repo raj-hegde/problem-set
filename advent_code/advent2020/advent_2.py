a7b5fb9668c5d68ee0c3f7cb2dea4ea7688a1900
rajeev <rajeevhegde11@gmail.com>
2022-07-03 18:07:44 +0530

    advent update

--- advent_code/advent2020/advent_2.py
+++ advent_code/advent2020/advent_2.py
@@ -1,22 +1,24 @@
+import readline
 puzzle2 = open("/home/rajeev/personal/advent_code/advent2020/adventpuzzle_2.txt", "r")
 temp = 0
 x = []
 count = []
 match = []
-while puzzle2 == EOFError:
-    break
-    line =  puzzle2.readline()        
+for i in range(1):
+    line = "1-3 c: ccpc"
     for i in line:
         if i.isalpha():
             match.append(i)
         if i.isdigit():
             count.append(i)
-    print(count, match)
-# for i in range(1, len(match)):
-#     if match[0] == match[i]:
-#         temp += 1
-# if temp == count[0] and count[1]:
-#     print("password valid")
-# else:
-#     print("password does not match")
+    #print(count, match)
+    for i in range(1, len(match)):
+        if match[0] == match[i]:
+            temp += 1
+            #print(temp)
+            if temp >= int(count[0]) and temp <= int(count[1]):
+                print("password valid")
+            else:
+                print("password does not match")
+    
 

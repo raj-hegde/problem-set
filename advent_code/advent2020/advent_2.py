fc4106cdfe123bf308f37c1c37cab4a86a397fbc
raj-hegde <rajeevhegde11@gmail.com>
2022-07-03 16:19:43 +0530

    edit to advent2

--- advent_code/advent2020/advent_2.py
+++ advent_code/advent2020/advent_2.py
@@ -1,18 +1,22 @@
 puzzle2 = open("/home/rajeev/personal/advent_code/advent2020/adventpuzzle_2.txt", "r")
+temp = 0
 x = []
 count = []
 match = []
-for i in range(10):
-    line = puzzle2.readline()
-    #print(line)
+while puzzle2 == EOFError:
+    break
+    line =  puzzle2.readline()        
     for i in line:
-        x.append(i)
-    for j in range(len(x)):
-        if x[j].isdigit():
-            count.append(x[j])
-        
-        if x[j].isalpha():
-            match.append(x[j])
+        if i.isalpha():
+            match.append(i)
+        if i.isdigit():
+            count.append(i)
+    print(count, match)
+# for i in range(1, len(match)):
+#     if match[0] == match[i]:
+#         temp += 1
+# if temp == count[0] and count[1]:
+#     print("password valid")
+# else:
+#     print("password does not match")
 
-
-print(count, match)

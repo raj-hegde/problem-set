e96ae0633c506d329890914ce25abbd478c2c9af
raj-hegde <rajeevhegde11@gmail.com>
2022-10-20 21:32:20 +0530

    update

---
+++ data_struct&algo/grade_point_avg.py
@@ -0,0 +1,7 @@
+print('Welcome to the GPA calculator')
+print('Enter all your letter grades, one per line')
+print('Enter a blank line to designate the end.')
+#map all letter grades to point values
+points = {'A+':4.0, 'A':4.0, 'A-':3.67, 
+
+}

fd6b80eae6e3aa10960fa9b7e1733da38fe75c54
raj-hegde <rajeevhegde11@gmail.com>
2022-11-16 23:24:49 +0530

    another problem solved

--- leetcode/roman_num.py
+++ leetcode/roman_num.py
@@ -1,5 +1,21 @@
-def numerals():
-    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        s = ["III"]
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            print(char)
+            number += translations[char]
+        return number
     
-
-numerals()

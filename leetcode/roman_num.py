0acdee3b8f3a8cfe4ce52134ff6cf7e0eb18d017
raj-hegde <rajeevhegde11@gmail.com>
2022-11-17 07:56:40 +0530

    romantoint code without self

--- leetcode/roman_num.py
+++ leetcode/roman_num.py
@@ -1,5 +1,5 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
+    def romanToInt(s):
         translations = {
             "I": 1,
             "V": 5,
@@ -9,13 +9,13 @@ class Solution:
             "D": 500,
             "M": 1000
         }
-        s = ["III"]
+        
         number = 0
         s = s.replace("IV", "IIII").replace("IX", "VIIII")
         s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
         s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
         for char in s:
-            print(char)
             number += translations[char]
         return number
-    
+s = ("MCMXCIV")    
+print(Solution.romanToInt(s))

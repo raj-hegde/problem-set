61e9659a6a57bc032873f590b7bcd16f62427c1a
rajeev <rajeevhegde11@gmail.com>
2023-01-31 21:03:13 +0530

    code working but not submitted on leet code

--- leetcode/tribonacci.py
+++ leetcode/tribonacci.py
@@ -1,10 +1,11 @@
 # Need to understand the class and its characteristics
-def tribonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1 or n == 2:
-        return 1
-    else:
-        return tribonacci(n-3) + tribonacci(n-1) + tribonacci(n-2)
+class Solution:
+    def tribonacci(n):
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        else:
+            return tribonacci(n-3) + tribonacci(n-1) + tribonacci(n-2)
 
 print(tribonacci(25))

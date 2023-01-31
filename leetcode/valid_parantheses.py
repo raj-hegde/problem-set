b76ec06cf622ac30f96327803270482a40a4c311
rajeev <rajeevhegde11@gmail.com>
2023-01-31 22:39:58 +0530

    updation

--- leetcode/valid_parantheses.py
+++ leetcode/valid_parantheses.py
@@ -2,21 +2,7 @@ class Solution:
     def isValid(s: str) -> bool:
         stack = []
         dict = {"(" : ")", "{" : "}", "[" : "]"}
-        for char in s:
-            try:
-                stack.append(dict[char])
-               # print(stack)
-            except KeyError:
-                pass
-        if not stack:
-            return False
-        else:
-            for char in stack:
-                if char in s:
-                    return True
-                else:
-                    return False
-
+        
 s = "([)]" #cases from leet code
 print(Solution.isValid(s))
 

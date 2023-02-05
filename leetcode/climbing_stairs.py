87a75b0ea5a61aae7295e58c4693a8d602c6f9bf
rajeev <rajeevhegde11@gmail.com>
2023-02-05 19:58:20 +0530

    understand different approach

--- leetcode/climbing_stairs.py
+++ leetcode/climbing_stairs.py
@@ -1,8 +1,26 @@
 class Solution:
-    def climbStairs(n:int) -> int:
+    def climbStairs(self, n:int) -> int:
+        self._n = n
+#         if n <= 2:
+#             return n
+#         else:
+#             dp = [0] * (n+1)
+#             dp[1] = 1
+#             dp[2] = 2
+#             for i in range(3, n+1):
+#                 dp[i] = dp[i-1] + dp[i-2]
+#             #print(dp)
+#         return dp[n]
+# print(climbStairs(4))
         if n <= 2:
             return n
         else:
-            return Solution.climbStairs(n-1) + Solution.climbStairs(n-2)
+            return Solution.climbStairs(self, n-1) + Solution.climbStairs(self, n-2)
+ 
+print(Solution.climbStairs(self,4))
 
-    print(climbStairs(4))
+
+
+
+
+    

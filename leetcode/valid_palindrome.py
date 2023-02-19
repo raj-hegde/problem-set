86bf825b67c5d0ca050514cd0ac6d8d04606c073
raj-hegde <rajeevhegde11@gmail.com>
2023-02-19 22:11:18 +0530

    update valid_palindrome

--- leetcode/valid_palindrome.py
+++ leetcode/valid_palindrome.py
@@ -1,9 +1,18 @@
-s = "A man, a plan, a canal: Panama"
-let_list = []
-if s.isalpha():
-  print(True)
-else:
-  for i in range(0, len(s)):
-    if s[i].isalpha():
-      let_list = s[i] 
-print(let_list)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+      let_list = ""
+      if s.isalpha() and s == s[::-1]:
+        return True
+      else:
+        for i in range(0, len(s)):
+          if s[i].isalpha():
+            let_list += s[i] #remove everything except alphabet
+      let_list = let_list.lower()
+      if let_list == let_list[::-1]:
+        return True
+      else:
+        return False
+
+s = "0P"
+self = None
+print(Solution.isPalindrome(self, s))

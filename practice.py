8cd23d5c0e69b4e904fe33ab80acfb227e217699
raj-hegde <rajeevhegde11@gmail.com>
2023-04-09 16:12:48 +0530

    python

--- practice.py
+++ practice.py
@@ -1,9 +1,15 @@
-t = int(input())
-print(t)
-sum = 0
-for a in range(t):
-    n = int(input().strip())
-    for j in range(n):
-        if j % 3 == 0 or j % 5 == 0:
-            sum += j
-print(sum)
+from collections import deque
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+#if __name__ == '__main__':
+    with open('test.txt') as f:
+        for line, prevlines in search(f, 'silently', 5):
+            for pline in prevlines:
+                print(pline, end=' ')
+            print(line, end=' ')
+            print('-'*20)

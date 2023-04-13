0acbca8158b2110241a0be0f2f06554463df24e4
rajeev <rajeevhegde11@gmail.com>
2023-04-13 16:40:54 +0530

    stars from strings

--- leetcode/remove_stars_from_string.py
+++ leetcode/remove_stars_from_string.py
@@ -1,6 +1,15 @@
 class Solution:
     def removeStars(self, s: str) -> str:
-        print(list(s))
+        temp = list(s)
+        for i in range(len(temp)-1):
+            if temp[i] == '*':
+                temp.pop(i)
+                temp.pop(i-1)
+                
+        print(temp)
+                
+
+
         
 self = None
 s = "erase*****"

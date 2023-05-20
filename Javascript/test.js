c6af35cba9e731df3e6c92ae6780a6b10f7649e1
rajeev <rajeevhegde11@gmail.com>
2023-05-20 14:51:06 +0530

    update

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+function findSolution(target){
+  function find(current, history){
+    if (current == target){
+      return history;
+    } else if (current > target){
+      return null;
+    }else{
+      return find((current + 5), `(${history} + 5)`) ||
+             find((current * 3), `(${history} * 3)`);
+    }
+  }
+  return find(1, "1");
+}
+
+console.log(findSolution(24));
+
+/* still need to understand how control flow works and how current is reducing after the second recursive call in
+return statement.
+*/

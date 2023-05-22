ee5121c73e6081183f914b84754b6ef43b84ff31
Rajeev <rajeevhegde11@gmail.com>
2023-05-23 00:02:49 +0530

    still need to fighure out

---
+++ Javascript/squirrel_journal.js
@@ -0,0 +1,59 @@
+let journal = [];
+
+function addEntry(events, squirrel) {
+    journal.push({events, squirrel});
+}
+
+addEntry(["work", "touched tree", "pizza", "running", "television"], false);
+addEntry(["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushed teeth"], false);
+addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true);
+
+//console.log(journal)
+
+function phi(table){
+    return (table[3] * table[0] - table[2] * table[1]) /
+      Math.sqrt((table[2] + table[3]) * 
+                (table[0] + table[1]) * 
+                (table[1] + table[3]) * 
+                (table[0] + table[2]));
+  }
+  
+  //console.log(phi([76, 9, 4, 1]));
+  
+  function tableFor(event, journal){
+    let table = [0, 0, 0, 0];
+    for (let i = 0; i < journal.length; i++){
+      let entry = journal[i], index = 0;
+      if (entry.events.includes(event)) index += 1;
+      if (entry.squirrel) index += 2;
+      table[index] += 1;
+    }
+    return table;
+  }
+  
+  console.log(tableFor("pizza", JOURNAL)); // need to understand JOURNAL variable, Right now it throws an error
+
+  function journalEvents(journal){
+    let events = [];
+    for (let entry of journal) {
+        for (let event of entry.events) {
+            if (!events.includes(event)) {
+                events.push(event);
+            }
+        }
+    }
+    return events;
+  }
+
+  console.log(journalEvents(JOURNAL));
+
+  for (let event of journalEvents(JOURNAL)) {
+    console.log(event + ":", phi(tableFor(event, JOURNAL)));
+  }
+
+for (let event of journalEvents(JOURNAL)) {
+    let correlation = phi(tableFor(event, JOURNAL));
+    if (correlation > 0.1 || correlation < -0.1){
+        console.log(event + ":", correlation);
+    }
+}

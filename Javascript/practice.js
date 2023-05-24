5e0783ec8befdc5d7ce94293e9a9f9aaf4592ec4
rajeev <rajeevhegde11@gmail.com>
2023-05-24 19:02:34 +0530

    "update"

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -1,10 +1,16 @@
-function deepEqual(a, b){
-    let KeysA = Object.keys(a), KeysB = Object.keys(b);
-    console.log(KeysA, KeysB);
-    for (let key of KeysA){
-        if (!KeysB.includes(key) || !deepEqual(a[key], b[key])) return false;
-    }
-}
+// function deepEqual(a, b){
+//     let KeysA = Object.keys(a), KeysB = Object.keys(b);
+//     console.log(KeysA, KeysB);
+//     for (let key of KeysA){
+//         if (!KeysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+//     }
+// }
 
-let obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
+// let obj = {here: {is: "an"}, object: 2};
+// console.log(deepEqual(obj, obj));
+
+let labels = [];
+repeat(5, i => {
+    return labels.push(`Unit ${i + 1}`);
+});
+console.log();

319727577f8e0eadcf8a6bc8a5a3e30da313c5bd
Rajeev <rajeevhegde11@gmail.com>
2023-05-29 01:20:44 +0530

    removeElement

--- Javascript/removeElement.js
+++ Javascript/removeElement.js
@@ -1,12 +1,6 @@
 var removeElement = function(nums, val){
-//let cnt = 0;
-    for (let i = 0; i < nums.length; i++){
-     if (val === nums[i]) {
-         nums.splice(i, 1);
-         i--;
-     }
-    }
-    return nums;
+  const newArr = nums.filter(i => i !== val);
+  return newArr;
 };
 console.log(removeElement([3,2,2,3], 3));
 console.log(removeElement([0,1,2,2,3,0,4,2], 2));

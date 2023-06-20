9be4ad8b4d1f1253636c062eb4d96ad487bc81f2
rajeev <rajeevhegde11@gmail.com>
2023-06-20 19:36:44 +0530

    update

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -89,10 +89,10 @@
 // };
 // console.log(stringObject[toStringSymbol]());
 
-var resultString = "<p>This page \u00A9 Shelly Powers </p>";
+// var resultString = "<p>This page \u00A9 Shelly Powers </p>";
 
-var blk = document.getElementById("result");
-blk.innerHTML = resultString;
+// var blk = document.getElementById("result");
+// blk.innerHTML = resultString;
 
 
 // var sentence = 'This is one sentence. This is a sentence with a list of items:' +
@@ -106,6 +106,59 @@ blk.innerHTML = resultString;
 
 // var fruits = listStr.split(',');
 // console.log(fruits);
+
+// Matrix class acting as a 2D Array
+class Matrix{
+  constructor(width, height, element = (x, y) => undefined){
+    this.width = width;
+    this.height = height;
+    this.content = [];
+
+    for (let y = 0; y < height; y++){
+      for (let x = 0; x < width; x++){
+        this.content[y * width + x] = element(x, y);
+      }
+    }
+  }
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
+  }
+}
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+
+  next(){
+    if (this.y == this.matrix.height) return {done: true};
+
+    let value = {x: this.x,
+                 y: this.y,
+                 value: this.matrix.get(this.x, this.y)};
+    this.x++;
+
+    if (this.x == this.matrix.width) {
+      this.x = 0;
+        this.y++;
+      }
+      return {value, done: false};
+  }
+}
+
+Matrix.prototype[Symbol.iterator] = function() {
+  return new MatrixIterator(this);
+};
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+for (let {x, y, value} of matrix){
+  console.log(x, y, value);
+}
 
 
 

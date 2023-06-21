96e6d973c422485849f3b2be8627e8a882abf1b3
rajeev <rajeevhegde11@gmail.com>
2023-06-21 18:23:07 +0530

    update

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -161,14 +161,33 @@
 // }
 // Matrix class ends here
 
-let varyingSize = {
-  get size() {
-    return Math.floor(Math.random() * 100);
+// let varyingSize = {
+//   get size() {
+//     return Math.floor(Math.random() * 100);
+//   }
+// };
+// console.log(varyingSize.size);
+
+class Temperature {
+  constructor(celsius) {
+      this.celsius = celsius;
   }
-};
-console.log(varyingSize.size);
+  get fahrenheit() {
+    return this.celsius * 1.8 + 32;
+  }
+  set fahrenheit(value) {
+    this.celsius = (value - 32) / 1.8;
+  }
+  static fromFahrenheit(value) {
+    return new Temperature((value - 32) / 1.8);
+  }
+}
 
+let temp = new Temperature(22);
+console.log(temp.fahrenheit);
 
+temp.fahrenheit = 86;
+console.log(temp.celsius);
 
 
 

2a4aca2be4424848df9b59d1ca8c15d224c1b2e3
rajeev <rajeevhegde11@gmail.com>
2023-07-05 11:19:28 +0530

    update

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -168,27 +168,26 @@
 // };
 // console.log(varyingSize.size);
 
-class Temperature {
-  constructor(celsius) {
-      this.celsius = celsius;
-  }
-  get fahrenheit() {
-    return this.celsius * 1.8 + 32;
-  }
-  set fahrenheit(value) {
-    this.celsius = (value - 32) / 1.8;
-  }
-  static fromFahrenheit(value) {
-    return new Temperature((value - 32) / 1.8);
-  }
-}
+// class Temperature {
+//   constructor(celsius) {
+//       this.celsius = celsius;
+//   }
+//   get fahrenheit() {
+//     return this.celsius * 1.8 + 32;
+//   }
+//   set fahrenheit(value) {
+//     this.celsius = (value - 32) / 1.8;
+//   }
+//   static fromFahrenheit(value) {
+//     return new Temperature((value - 32) / 1.8);
+//   }
+// }
 
-let temp = new Temperature(22);
-console.log(temp.fahrenheit);
+// let temp = new Temperature(22);
+// console.log(temp.fahrenheit);
 
-temp.fahrenheit = 86;
-console.log(temp.celsius);
-
+// temp.fahrenheit = 86;
+// console.log(temp.celsius);
 
 
 

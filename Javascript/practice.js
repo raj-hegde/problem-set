ac834d8482ff512e1f52dce2f6f928cea10b4c05
Rajeev <rajeevhegde11@gmail.com>
2023-07-15 09:14:08 +0530

    update

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -248,58 +248,58 @@
 
 // class Group
 
-class Group {
-    constructor() {
-        this.members = [];
-    }
+// class Group {
+//     constructor() {
+//         this.members = [];
+//     }
 
-    add(value) {
-        if(!this.has(value)) {
-            this.members.push(value);
-        }
-    }
+//     add(value) {
+//         if(!this.has(value)) {
+//             this.members.push(value);
+//         }
+//     }
 
-    delete(value) {
-        this.members = this.members.filter(v => v !== value);
-    }
+//     delete(value) {
+//         this.members = this.members.filter(v => v !== value);
+//     }
 
-    has(value){
-        return this.members.includes(value);
-    }
+//     has(value){
+//         return this.members.includes(value);
+//     }
 
-    static from(collection) {
-        let group = new Group;
-        for (let value of collection) {
-            group.add(value);
-        }
-        return group;
-    }
-    [Symbol.iterator]() {
-    return new GroupIterator(this);
-    }
-}
+//     static from(collection) {
+//         let group = new Group;
+//         for (let value of collection) {
+//             group.add(value);
+//         }
+//         return group;
+//     }
+//     [Symbol.iterator]() {
+//     return new GroupIterator(this);
+//     }
+// }
 
-class GroupIterator {
-    constructor(group) {
-        this.group = group;
-        this.position = 0;
-    }
+// class GroupIterator {
+//     constructor(group) {
+//         this.group = group;
+//         this.position = 0;
+//     }
 
-    next() {
-        if (this.position >= this.group.members.length) {
-            return {done: true};
-        } else {
-            let result = {value: this.group.members[this.position],
-                         done: false};
-            this.position++;
-            return result;
-        }
-    }
-}
+//     next() {
+//         if (this.position >= this.group.members.length) {
+//             return {done: true};
+//         } else {
+//             let result = {value: this.group.members[this.position],
+//                          done: false};
+//             this.position++;
+//             return result;
+//         }
+//     }
+// }
 
-for (let value of Group.from(["a", "b", "c"])) {
-    console.log(value);
-}
+// for (let value of Group.from(["a", "b", "c"])) {
+//     console.log(value);
+// }
 
 
 
@@ -308,6 +308,14 @@ for (let value of Group.from(["a", "b", "c"])) {
 
 
 // Iterable groups
+
+x = new Date(Date.UTC(2011, 3, 25))
+console.log(x.toUTCString());
+if  (Date.parse('1991-03-27T01:46:40Z') === Date.UTC((1959, 6, 19))){ 
+    console.log(true)
+} else {console.log(false)}
+
+
 
 
 

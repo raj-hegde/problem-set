b276ff3a14ee41848079967f20c9b3e5bfbe4007
Rajeev <rajeevhegde11@gmail.com>
2023-09-26 23:42:41 +0530

    karatsuba update

--- typescript/karatsuba.js
+++ typescript/karatsuba.js
@@ -1,9 +1,13 @@
+//split the number
 function digDiv(n) {
     let h = n % 100;
     let j = Math.floor(n / 100);
-    console.log(j, h);
+    return [j, h];
 }
-digDiv(1023);
 function karatsuba(x, y) {
+    const [a, b] = digDiv(x);
+    const [c, d] = digDiv(y);
+    console.log(a, b, c, d);
 }
+karatsuba(8787, 9825);
 //# sourceMappingURL=karatsuba.js.map
--- typescript/karatsuba.ts
+++ typescript/karatsuba.ts
@@ -1,13 +1,16 @@
-function digDiv(n: number){
-    let h: number = n % 100;
-    let j: number = Math.floor(n / 100);
-    console.log(j, h)
+//split the number
+function digDiv(n: number) {
+  let h: number = n % 100;
+  let j: number = Math.floor(n / 100);
+
+  return [j, h];
 }
-
-digDiv(1023)
-
 
 function karatsuba(x: number, y: number) {
-    
+  const [a, b] = digDiv(x);
+  const [c, d] = digDiv(y);
+
+  console.log(a, b, c, d);
 }
 
+karatsuba(8787, 9825);

20a0b436f710dd913d0125c5618f569cf1a5c771
rajeev <rajeevhegde11@gmail.com>
2023-09-26 19:56:32 +0530

    algo in typescript

---
+++ typescript/karatsuba.js
@@ -0,0 +1,9 @@
+function digDiv(n) {
+    let h = n % 100;
+    let j = Math.floor(n / 100);
+    console.log(j, h);
+}
+digDiv(1023);
+function karatsuba(x, y) {
+}
+//# sourceMappingURL=karatsuba.js.map

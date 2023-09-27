2358e94bfe35c4731ce4332f1ca60f454373595a
rajeev <rajeevhegde11@gmail.com>
2023-09-27 19:13:38 +0530

    mergesort

---
+++ typescript/mergesort.js
@@ -0,0 +1,8 @@
+function mergesort(arr) {
+    //splitting the array
+    let arr1 = arr.slice(0, arr.length / 2);
+    let arr2 = arr.slice(arr.length / 2, arr.length);
+    return;
+}
+console.log(mergesort([5, 4, 1, 8, 7, 2, 6, 3]));
+//# sourceMappingURL=mergesort.js.map
---
+++ typescript/mergesort.ts
@@ -0,0 +1,14 @@
+function mergesort(arr: number[]): number[] {
+
+    //splitting the array
+    let arr1: number[] = arr.slice(0, arr.length/2);
+    let arr2: number[] = arr.slice(arr.length/2, arr.length);
+    
+
+    
+    return 
+}
+
+
+
+console.log(mergesort([5, 4, 1, 8, 7, 2, 6, 3]))

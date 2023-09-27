8786dde9aacbc204b26e32351efd3e9c7dd19a90
rajeev <rajeevhegde11@gmail.com>
2023-09-27 13:26:45 +0530

    karatsuba update

--- typescript/karatsuba.js
+++ typescript/karatsuba.js
@@ -7,7 +7,20 @@ function digDiv(n) {
 function karatsuba(x, y) {
     const [a, b] = digDiv(x);
     const [c, d] = digDiv(y);
-    console.log(a, b, c, d);
+    const n = Math.ceil(Math.log10(x)); // length of either x or y
+    if (n === 1) {
+        return x * y;
+    }
+    else {
+        var ac = a * c;
+        var bd = b * d;
+        let p = a + b;
+        let q = c + d;
+        let pq = p * q;
+        var adbc = pq - ac - bd;
+    }
+    const ans = (Math.pow(10, n) * ac) + ((Math.pow(10, (n / 2))) * adbc) + bd;
+    return ans;
 }
-karatsuba(8787, 9825);
+console.log(karatsuba(1, 7));
 //# sourceMappingURL=karatsuba.js.map
--- typescript/karatsuba.ts
+++ typescript/karatsuba.ts
@@ -10,7 +10,24 @@ function karatsuba(x: number, y: number) {
   const [a, b] = digDiv(x);
   const [c, d] = digDiv(y);
 
-  console.log(a, b, c, d);
+  const n: number = Math.ceil(Math.log10(x)); // length of either x or y
+  
+  if (n === 1) {
+    return x * y;
+  }
+  else {
+    var ac: number = a * c;
+    var bd: number = b * d;
+    let p: number = a + b;
+    let q: number = c + d;
+    let pq: number = p * q;
+    var adbc: number = pq - ac - bd;
+  }
+
+  const ans: number = (Math.pow(10, n) * ac) + ((Math.pow(10, (n / 2))) * adbc) + bd;
+  return ans;
 }
 
-karatsuba(8787, 9825);
+
+console.log(karatsuba(1, 7));
+

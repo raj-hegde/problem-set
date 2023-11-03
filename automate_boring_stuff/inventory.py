8d28763eb0d42b091bb6476aa2c721c2f3f503ea
Rajeev <rajeevhegde11@gmail.com>
2023-11-03 20:19:27 +0530

    update automate boring stuff

--- automate_boring_stuff/inventory.py
+++ automate_boring_stuff/inventory.py
@@ -6,15 +6,22 @@ def display_inventory(inventory):
     for k, v in inventory.items():
         print(str(v) + ' ' + k)
         item_total += v
-    print("Total number of items: " + str(item_total))
+    print("\nTotal number of items: " + str(item_total))
 
-display_inventory(stuff)
+#display_inventory(stuff)
 
-def add_to_inventory():
-    
+def add_to_inventory(inventory, addeditmes):
+    for item in addeditmes:
+        if item in inventory.keys():    # checking if item exists in inventory
+            inventory[item] = inventory.get(item, 0) + 1
+        else:
+            inventory.setdefault(item, 1)
+    return inventory
+
 
 inv = {'gold coin': 42, 'rope': 1}
 dragonloot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
 inv = add_to_inventory(inv, dragonloot)
 display_inventory(inv)
 

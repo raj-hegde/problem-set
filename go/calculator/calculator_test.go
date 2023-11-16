3b857b5db0ea09861fc2b1a353523322564ca8f4
Rajeev Hegde <rajeevhegde11@gmail.com>
2023-11-16 19:18:30 +0530

    progressing go

--- go/calculator/calculator_test.go
+++ go/calculator/calculator_test.go
@@ -13,3 +13,20 @@ func TestAdd(t *testing.T) {
 		t.Errorf("want %f, got %f", want, got)
 	}
 }
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(4, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 9
+	got := calculator.Multiply(3, 3)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}

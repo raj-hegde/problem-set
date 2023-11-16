d7e0a0703929649ddd50ff0610b79180c01d408f
Rajeev Hegde <rajeevhegde11@gmail.com>
2023-11-16 19:18:49 +0530

    progresing go

--- go/calculator/calculator.go
+++ go/calculator/calculator.go
@@ -6,3 +6,11 @@ package calculator
 func Add(a, b float64) float64 {
 	return a + b
 }
+
+func Subtract(a, b float64) float64 {
+	return a - b
+}
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
